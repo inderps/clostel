d7e5fee446ab97b2623edaebccdee1fd3a688cb8
Inderpal Singh <inderpal@pfeffermind-games.de>
2015-07-22 00:27:04 +0530

    basic landing page is ready

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,3 @@
 $(document).ready(function(){
-  $('body').backstretch('images/bg.jpg');
-
-  function initialize() {
-    var myLatlng = new google.maps.LatLng(52.4965481, 13.4380442);
-    var mapCanvas = document.getElementById('map-canvas');
-    var mapOptions = {
-      center: myLatlng,
-      zoom: 14,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(mapCanvas, mapOptions)
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title: 'Hannah & Cash'
-    });
-  }
-  google.maps.event.addDomListener(window, 'load', initialize);
+  $('.main').backstretch('images/bg2.jpg');
 });
