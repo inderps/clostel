68630de268fe8209475f986cbbc3e6bb9b1ee8f9
Inderpal Singh <inderpal@pfeffermind-games.de>
2015-07-27 17:04:10 +0530

    more anayltics

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,30 @@
 $(document).ready(function(){
   $('.main').backstretch('images/bg.jpg');
   $('#glimpse').backstretch('images/glimpse.jpg');
+
+
+  $.get("http://ipinfo.io", function(response) {
+    $('#hidden_div').prepend("<img src='http://tunescart.com/clostel/visited?ip="+ response.ip + "' />");
+    $('#ip').val(response.ip);
+  }, "jsonp");
+
+
+  var showAlert = function(){
+    var vars = [], hash;
+    var q = document.URL.split('?')[1];
+    if(q != undefined){
+      q = q.split('&');
+      for(var i = 0; i < q.length; i++){
+        hash = q[i].split('=');
+        vars.push(hash[1]);
+        vars[hash[0]] = hash[1];
+      }
+    }
+
+    return vars['message'] != null;
+  };
+
+  if(showAlert()){
+    $('#myModal').modal('show');
+  }
 });
