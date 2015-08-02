5474ffe000e296c4a9238112ba0f204fb34bd970
Inderpal Singh <inderpal@pfeffermind-games.de>
2015-08-02 20:16:55 +0530

    dist cleanup

---
+++ app/assets/js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+  $.get("http://ipinfo.io", function(response) {
+    $('#hidden_div').prepend("<img src='http://tunescart.com/clostel/visited?ip="+ response.ip + "' />");
+    $('#ip').val(response.ip);
+  }, "jsonp");
+
+
+  var showAlert = function(){
+    var vars = [], hash;
+    var q = document.URL.split('?')[1];
+    if(q != undefined){
+      q = q.split('&');
+      for(var i = 0; i < q.length; i++){
+        hash = q[i].split('=');
+        vars.push(hash[1]);
+        vars[hash[0]] = hash[1];
+      }
+    }
+
+    return vars['message'] != null;
+  };
+
+  if(showAlert()){
+    $('#myModal').modal('show');
+  }
+
+  window.setInterval(function(){
+    $('#signupModal').modal('show');
+  }, 10000);
+
+
+  $('a').click(function(ev){
+    if($(ev.currentTarget).attr('href') == '#showPopup'){
+      $('#signupModal').modal('show');
+    }
+  });
+
+});
